4f62f41d68c72801c6c4822a4cc15e90c2d80c62
Tolga Köyluoglu <tolga.koyluoglu@chasacademy.se>
2019-02-06 18:36:04 +0100

    Add recipe services and methods for http request, add input field in the view. results shows in console on searchstrings now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'U06 - Frontend';
+  title = 'Recipe App';
+  searchInput = this.searchInput;
+
+
+  constructor(private recipeService: RecipeService) { }
+
+
+  handleRecipes = () => {
+    this.recipeService.getRecipes(this.searchInput).subscribe(data => {
+      console.log(data);
+    })
+  }
 }
--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class RecipeService {
 
-  constructor() { }
+  baseUrl = `https://api.edamam.com/`
+  apiId = `4e1fec26`
+  apiKey = `700a8df20bec59685c5284880e20ef03`
+
+
+  constructor(private http: HttpClient) { }
+
+  getRecipes(
+    results: string
+  ) {
+
+    return this.http.get<any>(this.baseUrl + 'search?q=' + results + '&app_id=' + this.apiId + '&app_key=' + this.apiKey)
+  }
 }
