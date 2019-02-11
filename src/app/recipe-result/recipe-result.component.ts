922278b1807bb52db9f6f10fe340bb38b6545e7c
Tolga Köyluoglu <tolga.koyluoglu@chasacademy.se>
2019-02-11 15:28:12 +0100

    Reseted to previous commit and fixed code

---
+++ src/app/recipe-result/recipe-result.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-result',
+  templateUrl: './recipe-result.component.html',
+  styleUrls: ['./recipe-result.component.scss']
+})
+export class RecipeResultComponent implements OnInit {
+
+  searchInput = this.searchInput;
+  recipes: [];
+
+
+  constructor(private recipeService: RecipeService) { }
+
+
+  handleRecipes = () => {
+    this.recipeService.getRecipes(this.searchInput).subscribe(data => {
+      this.recipes = data.hits.map(hit => hit.recipe)
+      console.log(data.hits)
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
