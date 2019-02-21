42f8c77ad88edb9cebc5044d7f7e93e9e7d7e631
Tolga Köyluoglu <tolga.koyluoglu@chasacademy.se>
2019-02-21 13:24:15 +0100

    Added before/after log in services so you cant access certain pages depending on logged in or not

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,15 @@ import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ResponsePasswordComponent } from './response-password/response-password.component';
+import { BeforeLoginService } from './before-login.service';
+import { AfterLoginService } from './after-login.service';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'response-password', component: ResponsePasswordComponent },
+  { path: 'login', component: LoginComponent, canActivate: [BeforeLoginService] },
+  { path: 'signup', component: SignupComponent, canActivate: [BeforeLoginService] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AfterLoginService] },
+  { path: 'reset-password', component: ResetPasswordComponent, canActivate: [AfterLoginService] },
+  { path: 'response-password', component: ResponsePasswordComponent, canActivate: [AfterLoginService] },
   { path: 'recipes', component: RecipeResultComponent },
   { path: 'recipes/detail/:id', component: RecipeDetailsComponent },
   { path: 'saved', component: SavedComponent },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { TokenService } from './token.service';
 
 
 @Component({
@@ -13,11 +15,20 @@ export class AppComponent {
   public loggedIn: boolean;
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router,
+    private token: TokenService
   ) { }
 
   ngOnInit() {
     this.auth.authStatus.subscribe(value => this.loggedIn = value);
+  }
+
+  logout(event: MouseEvent) {
+    event.preventDefault();
+    this.token.remove();
+    this.auth.changeStatus(false);
+    this.router.navigateByUrl('/login');
   }
 
 }
